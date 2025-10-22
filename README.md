6f0c26fc2f0bbead88ebe51f9bd9dbb344e643ff
LorienWood <you@example.com>
2025-10-22 22:29:34 +0100

    Added README.md

---
+++ README.md
@@ -0,0 +1,38 @@
+# Express Web Application 
+A small web application built using Express.js. This project 
+
+## Technologies Used 
+- Express.js – for handling web requests and routes
+- Node.js – to run the server
+- HTML – for structing the webpages 
+
+## How to Install and Run Locally 
+
+1. Clone the Github 
+```
+git clone https://github.com/lorienmwood/04_express_33827617.git
+cd 04_express_33827617
+```
+2. Install Node.js
+```
+npm install
+```
+3. Run the Server 
+```
+node index.js
+```
+4. Open the Browser 
+<br>
+Visit http://localhost:8000/ and the webpage will open. 
+
+
+## Routes 
+
+| Route             | Description   |
+| ----------------- | ------------- |
+| /                 | Home route
+| / about            | About route 
+| / contact          | Contact route
+| / date             | Current time and date route 
+| / welcome:name     |'Welcome User's Name' route 
+| / chain            | 
--- routes/main.js
+++ routes/main.js
@@ -3,33 +3,44 @@ const express = require("express");
 const router = express.Router();
 
 // Handle the main routes
-
 router.get("/", (req, res) => res.send("Hello World!"));
 
+// About Page Route
 router.get("/about", (req, res) => res.send("<h1>This is the about page</h1>"));
 
+// Contact Page Route
 router.get("/contact", (req, res) =>
   res.send("<h1>This is the contact page</h1>")
 );
 
+// Date Page Route
 router.get("/date", (req, res) => {
+  // A date variable set as current date and time
   const date = new Date();
+
+  // Formats the date/time to make readable
   const todaysDate = date.toLocaleString();
   res.send(`<h1> Todays Date is: ${todaysDate}</h1>`);
 });
 
+/* Welcome Page Route with user inputting a name
+e.g. http://www.doc.gold.ac.uk/usr/153/welcome/lorien  */
 router.get("/welcome/:name", (req, res) => {
-  const userName = req.params.name;
-  res.send(`<h1>Welcome ${userName}</h1>`);
+  // A usersName variable storing name from url
+  const usersName = req.params.name;
+
+  // Inserts URL name
+  res.send(`<h1>Welcome ${usersName}</h1>`);
 });
 
+// Chain Route
 router.get("/chain", (req, res, next) => {
   console.log("Accessing the secret section ...");
   next(); // pass control to the next handler
 });
-router.get("/chain", (req, res,) => {
+router.get("/chain", (req, res) => {
   console.log("Accessing the secret section ...");
-  res.send("<h1>Chained Route</h1>")
+  res.send("<h1>Chained Route</h1>");
 });
 
 // Export the router object so index.js can access it
